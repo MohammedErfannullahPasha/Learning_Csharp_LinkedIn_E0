0cf77b93fb588e359808d6c6bf53217059104ccd
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-05-07 05:23:45 +0530

    challege2 and solution2 : type2

---
+++ AuthenticationPasscode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AuthenticationPasscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is the passcode ?");
+            var passcode = Console.ReadLine();
+            if (passcode == "secret")
+            {
+                Console.WriteLine("you are Authenticated");
+            }
+            else
+            {
+                Console.WriteLine("Invalid passcode");
+            }
+
+        }
+    }
+}

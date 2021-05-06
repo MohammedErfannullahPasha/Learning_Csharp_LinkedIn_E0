bceaad10617507c211260dfce214c4acbca461df
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-05-07 05:16:43 +0530

    challenge2 and solution2 : type1

--- SurveyApp/Program.cs
+++ SurveyApp/Program.cs
@@ -6,16 +6,41 @@ namespace SurveyApp
     {
         static void Main(string[] args)
         {
-            //Creating a survey application//
-            Console.WriteLine("what is your name ?");
-            string Name = Console.ReadLine();
-            Console.WriteLine("what is your age ?");
-            string Age = Console.ReadLine();
-            Console.WriteLine("what month were you born in ?");
-            string Month = Console.ReadLine();
+            string passcode = "secret";
+            Console.WriteLine("Please provide the passcode");
 
-            Console.WriteLine("Hi {0}! Since you were born in {1} and your " +
-                "age is {2}. you are elligible", Name,Month,Age);
+            if (passcode == Console.ReadLine())
+            {
+                Console.WriteLine("what is your name ?");
+                string Name = Console.ReadLine();
+                Console.WriteLine("what is your age ?");
+                string Age = Console.ReadLine();
+                Console.WriteLine("what month were you born in ?");
+                string Month = Console.ReadLine();
+                Console.WriteLine("Hi {0}! Since you were born in {1} and your " +
+                    "age is {2}", Name, Month, Age);
+
+                if (Month == "December")
+                {
+                    string Zodiac = "capricorn";
+                    Console.WriteLine(Zodiac);
+                }
+                else if (Month == "August")
+                {
+                    string Zodiac = "Leo";
+                    Console.WriteLine(Zodiac);
+                }
+                else if (Month == "April")
+                {
+                    string Zodiac = "Aries";
+                    Console.WriteLine(Zodiac);
+                }
+                                
+            }
+            else
+            {
+                Console.WriteLine("Wrong passcode, Please Exit !");
+            }
         }
     }
 }

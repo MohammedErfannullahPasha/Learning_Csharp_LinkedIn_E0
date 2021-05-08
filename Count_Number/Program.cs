dfe30aac438b374fa48f56f72c95d31cfbc9a906
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-05-08 15:41:20 +0530

    counting number using while loop : Type1

---
+++ Count_Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Count_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numb = 1;
+            while (numb <= 100)
+            {
+                Console.WriteLine(numb);
+                numb++;
+            }
+        }
+    }
+}

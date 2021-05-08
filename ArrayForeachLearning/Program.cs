c001153ad7392447246ef6c790b78dc1908a3514
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-05-08 17:35:21 +0530

    displaying all array values using foreach loop

---
+++ ArrayForeachLearning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArrayForeachLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new int[] { 98, 96, 67, 34, 56, 79, 100, 88 };
+            foreach (var studentGrade in studentGrades)
+            {
+                Console.WriteLine(studentGrade);
+            }
+
+        }
+    }
+}

892cb1f1c5ad333982fe4b5406a1482b515935b4
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-05-08 16:15:15 +0530

    Loop counting,this is what I understand : Type 1

---
+++ Loop_Counting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Loop_Counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            i--;
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            i++;
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            i--;
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            i++;
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

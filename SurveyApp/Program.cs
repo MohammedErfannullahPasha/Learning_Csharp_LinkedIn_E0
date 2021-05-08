4f2183d33e90d854f89356d73a1e2703ba3cd5cb
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-05-09 00:25:19 +0530

    introduction to functions or methods

--- SurveyApp/Program.cs
+++ SurveyApp/Program.cs
@@ -6,41 +6,59 @@ namespace SurveyApp
     {
         static void Main(string[] args)
         {
-            string passcode = "secret";
-            Console.WriteLine("Please provide the passcode");
 
-            if (passcode == Console.ReadLine())
-            {
-                Console.WriteLine("what is your name ?");
-                string Name = Console.ReadLine();
-                Console.WriteLine("what is your age ?");
-                string Age = Console.ReadLine();
-                Console.WriteLine("what month were you born in ?");
-                string Month = Console.ReadLine();
-                Console.WriteLine("Hi {0}! Since you were born in {1} and your " +
-                    "age is {2}", Name, Month, Age);
+            Console.WriteLine("what is your name ?");
+            string Name = Console.ReadLine();
 
-                if (Month == "December")
-                {
-                    string Zodiac = "capricorn";
-                    Console.WriteLine(Zodiac);
-                }
-                else if (Month == "August")
-                {
-                    string Zodiac = "Leo";
-                    Console.WriteLine(Zodiac);
-                }
-                else if (Month == "April")
-                {
-                    string Zodiac = "Aries";
-                    Console.WriteLine(Zodiac);
-                }
-                                
-            }
-            else
+            while (Name == "")
             {
-                Console.WriteLine("Wrong passcode, Please Exit !");
+                TryAgain();
+                Name = Console.ReadLine();
             }
+
+            Console.WriteLine("what is your age ?");
+            string Age = Console.ReadLine();
+
+            while (Age == "")
+            {
+                TryAgain();
+                Age = Console.ReadLine();
+            }
+
+            Console.WriteLine("what month were you born in ?");
+            string Month = Console.ReadLine();
+
+            while (Month == "")
+            {
+                TryAgain();
+                Month = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hi {0}! Since you were born in {1} and your " +
+                "age is {2}", Name, Month, Age);
+
+            if (Month == "December")
+            {
+                string Zodiac = "capricorn";
+                Console.WriteLine(Zodiac);
+            }
+            else if (Month == "August")
+            {
+                string Zodiac = "Leo";
+                Console.WriteLine(Zodiac);
+            }
+            else if (Month == "April")
+            {
+                string Zodiac = "Aries";
+                Console.WriteLine(Zodiac);
+            }
+
         }
+
+        static void TryAgain()
+        {
+            Console.WriteLine("Please provide the input, it is mandatory");
+        }
+
     }
 }

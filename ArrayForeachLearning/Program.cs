ed480af28e44c2506cc46fcdf39556c8cd4e4a62
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-05-19 17:05:16 +0530

    refactored the program using class and object for student list creation

--- ArrayForeachLearning/Program.cs
+++ ArrayForeachLearning/Program.cs
@@ -9,39 +9,55 @@ namespace ArrayForeachLearning
         static void Main(string[] args)
         {
 
-            var studentName = new List<string>();
-            var studentGrade = new List<int>();
+            var StudentList = new List<Student>();
+
             bool answer = true;
             while (answer)
             {
+                Student studentObj = new Student();
+
                 Console.WriteLine("Student_Name : ");
-                studentName.Add(Console.ReadLine());
+                studentObj.Name = Console.ReadLine();
 
                 Console.WriteLine("Student_Grade : ");
-                studentGrade.Add(int.Parse(Console.ReadLine()));
+                studentObj.Grade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Student_Bday : ");
+                studentObj.Birthday = Console.ReadLine();
+
+                Console.WriteLine("Student_Address : ");
+                studentObj.Address = Console.ReadLine();
+
+                Console.WriteLine("Student_Phone# : ");
+                studentObj.Phone = long.Parse(Console.ReadLine());
 
                 Console.WriteLine("Do you want to add another student details, Choose Yes/No ?");
                 string _answer = Console.ReadLine();
-                if (_answer == "Yes")
-                {
-                    answer = true;
-                }
-                else if (_answer == "No")
+
+                if (_answer != "Yes")
                 {
                     answer = false;
                 }
-                else
-                {
-                    Console.WriteLine("Please provide defined input");
-                }
+
+                StudentList.Add(studentObj);
 
             }
 
-            for (int i = 0; i < studentName.Count; i++)
+            foreach (var student in StudentList)
             {
-                Console.WriteLine("Student_Name : {0} & Student_Grade : {1}", studentName[i], studentGrade[i]);
+                Console.WriteLine("Student_Name : {0} | Student_Grade : {1} | Student_Bday : {2} | " +
+                    "Student_Address : {3} | Student_Phone# : {4} ", student.Name, student.Grade, student.Birthday, student.Address, student.Phone);
             }
 
         }
+    }
+
+    class Student
+    {
+        public string Name;
+        public int Grade;
+        public string Birthday;
+        public string Address;
+        public long Phone;
     }
 }

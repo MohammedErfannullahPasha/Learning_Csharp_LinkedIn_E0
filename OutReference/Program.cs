bcd69bb1aa767afeec37c84afe4f647047be3070
Erfan <mohammedpasha1993@outlook.com>
2021-07-26 00:34:36 +0530

    Type1 : create a method and any value passed into that method makes it set value of the created method.

---
+++ OutReference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OutReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            
+            Console.WriteLine(x);
+            
+            five(x);
+
+            Console.WriteLine(x);
+        }
+
+        //method that makes any value passed to parameter to value 5
+        public static void five (int a)
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }
+
+    }
+}

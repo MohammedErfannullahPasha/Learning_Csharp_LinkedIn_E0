0d783defbf358d77122529a028d8d36ee5743da1
Erfan <mohammedpasha1993@outlook.com>
2021-07-26 00:45:34 +0530

    Type2 : make local data of the variable to be changed using out keyword.

--- OutReference/Program.cs
+++ OutReference/Program.cs
@@ -10,13 +10,17 @@ namespace OutReference
             
             Console.WriteLine(x);
             
-            five(x);
+            five(out x);
+
+            //now, x is referencing towards variable "a" so the value provided by next line is 5
 
             Console.WriteLine(x);
         }
 
         //method that makes any value passed to parameter to value 5
-        public static void five (int a)
+        //to pass value 5 to the variable x, we need to reference variable "a" to x by using out keyword
+
+        public static void five (out int a)
         {
             a = 5;
             Console.WriteLine(a);

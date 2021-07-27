a63f987389c910558484890e615e89aa9463385e
Erfan <mohammedpasha1993@outlook.com>
2021-07-27 15:26:25 +0530

    Type2 :  adding the list of all the payees to be paid using List and interface

--- SchoolMembers/PayRoll.cs
+++ SchoolMembers/PayRoll.cs
@@ -9,22 +9,34 @@ namespace SchoolMember
     
     //we cannot add Pay() method in Memberofschool method becasue that will make students to inherit the same.
     //we can use interface which imposes to define inherited method i.e., pay()
+    //IPayee interface unifies all the payee classes
     interface IPayee
     {
         public void pay();
     }
 
-    class PayRoll
+    public class PayRoll
     {
-        Teacher teacher1 = new Teacher();
-        Teacher teacher2 = new Teacher();
-        Principal principal = new Principal();
+        
+        //imagine the payees increased in number and types of payee are also different ?
+        //we can create a list of payees and can add any class (type of payee) that uses Ipayee interface. 
+        List<IPayee> payees = new List<IPayee>();
+
+        //create a constructor and add the instances 
+        public PayRoll()
+        {
+            //adding instances of payees to list
+            payees.Add(new Teacher());
+            payees.Add(new Principal());
+        }
 
         public void PayAll()
         {
-            teacher1.pay();
-            teacher2.pay();
-            principal.pay();
+            foreach (var payee in payees)
+            {
+                payee.pay();
+            }
+
         }
     }
 }

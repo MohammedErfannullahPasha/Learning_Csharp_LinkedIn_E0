07fcddc35199ffd3fdfe3011ba0002e2ba325386
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-08-06 22:04:44 +0530

    Type1 : created a simple program to calculate area. here GetArea is a duplicate code

---
+++ GeometryTool/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GeometryTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Geometry Tool");
+            var square = new Square() { Side = 2 };
+            var triangle = new Triangle() { Height = 2, Width =5 };
+
+            Console.WriteLine(square.GetArea());
+            Console.WriteLine(triangle.GetArea());
+        
+        
+        
+        }
+    }
+
+    class Square
+    {
+        public int Side;
+
+        public int GetArea()
+        {
+            return Side * Side;
+        }
+    }
+
+    class Triangle
+    {
+        public int Height, Width;
+
+        public int GetArea()
+        {
+            return Height * Width / 2;
+        }
+    }
+
+}

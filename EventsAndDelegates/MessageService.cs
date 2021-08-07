280a3d3069ff6a47188548205d479e9db099802a
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-08-08 02:03:53 +0530

    Added message service to the EventsAndDelegates Programme

---
+++ EventsAndDelegates/MessageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class MessageService
+    {
+        //Event handler
+        public void SubVideoEncodedMessage(object source, EventArgs args)
+        {
+            Console.WriteLine("Message Service : Sending a test message....");
+        }
+    }
+}
--- EventsAndDelegates/Program.cs
+++ EventsAndDelegates/Program.cs
@@ -11,9 +11,11 @@ namespace EventsAndDelegates
             
             var vidEncode = new VideoEncoder(); //publisher
             var mailService = new MailService(); //Subscriber
+            var messageService = new MessageService(); //Subscriber
 
             //subsriber should subscribe/register the event before it begin or else, it will be null and user will face null exception.
             vidEncode.VideoEncoded += mailService.SubVideoEncodedMail;
+            vidEncode.VideoEncoded += messageService.SubVideoEncodedMessage;
             
             vidEncode.Encode(video);
 

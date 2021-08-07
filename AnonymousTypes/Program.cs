70e1a342cc4c6f13c8c8a74cf4a1e844746ecfde
MohammedErfannullahPasha <mohammedpasha1993@outlook.com>
2021-08-07 12:36:30 +0530

    understanding Anonymous types

---
+++ AnonymousTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learning Anonymous Types");
+
+            //below is the synctax of creating Anonymous type and it is really usefull
+            //when you need for temporary purpose and when you dont need to create a class
+
+            var Anonymoustype = new { Name = "Mohammed Erfannullah Pasha", age = 27 };
+
+            Console.WriteLine("Hi {0}, Your age is {1}", Anonymoustype.Name, Anonymoustype.age);
+
+        }
+    }
+}
